9b12652ec0685db8adc88b4d941bd92aab04cd3d
Neila Merizalde <nm.merizaldem@uea.edu.ec>
2025-02-26 12:46:28 -0500

    Primer commit del proyecto Flask

--- app.py
+++ app.py
@@ -1,28 +1,14 @@
-from flask import Flask, request
+from flask import Flask, render_template
+
 app = Flask(__name__)
+
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World clase encuentro!'
-# Nueva ruta para mostrar un mensaje personalizado
-@app.route('/usuario/<nombre>')
-def usuario(nombre):
-    return f'Bienvenido, {nombre}!'
-# Ruta para mostrar el formulario
-@app.route('/usuario/', methods=['GET', 'POST'])
-def usuario_form():
-    if request.method == 'POST':
-        nombre = request.form['nombre']
-        return f'Bienvenido, {nombre}!'
-    return '''
-        <form method="post">
-            <label for="nombre">Ingresa tu nombre:</label>
-            <input type="text" id="nombre" name="nombre">
-            <button type="submit">Enviar</button>
-        </form>
-    '''
-# Nueva ruta About
+def index():
+    return render_template('index.html')
+
 @app.route('/about')
 def about():
-    return 'Este es un proyecto de Flask para aprender a crear rutas y formularios.'
+    return render_template('about.html')
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
